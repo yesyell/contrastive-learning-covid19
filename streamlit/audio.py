66457cd09018c3d2bd41f07c11f7d7c7990f83a4
Seoyeon Oh <sohtks@swu.ac.kr>
2024-10-05 10:25:39 +0900

    modified: factor 숫자 변경

--- streamlit/audio.py
+++ streamlit/audio.py
@@ -3,8 +3,8 @@ from streamlit_webrtc import AudioProcessorBase
 
 
 # 볼륨 조절 함수 구현
-def change_volume(data, factor=1.0):
-    return data * factor
+def change_volume(data, factor=1.5):
+    return np.clip(data * factor, -1, 1)
 
 # AudiooProcessor 클래스 구현 (for streamlit-webrtc)
 class AudioProcessor(AudioProcessorBase):
